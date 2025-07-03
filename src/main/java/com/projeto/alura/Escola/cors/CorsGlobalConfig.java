10dcf369cbabfdb2d3cf83bfafbee4538b5c6846
ArturLLopes <arturllopes@gmail.com>
2025-07-03 13:56:38 -0300

    fix: url cors

--- src/main/java/com/projeto/alura/Escola/cors/CorsGlobalConfig.java
+++ src/main/java/com/projeto/alura/Escola/cors/CorsGlobalConfig.java
@@ -14,7 +14,7 @@ public class CorsGlobalConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080/swagger-ui/index.html#/")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
